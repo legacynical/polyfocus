cf09f8348c8677bfaa2fe30e5b5ab6ea3231b32e
Legacynical <legacynical@gmail.com>
2024-09-11 19:44:08 -0700

    add some audio sfx

---
+++ globals/audio manager/audio_manager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@onready var click_basic: AudioStreamPlayer = $ClickBasic
+@onready var timer_complete: AudioStreamPlayer = $TimerComplete
+@onready var _40_hz_focus: AudioStreamPlayer = $"40hzFocus" # L250hz R290hz
+@onready var _40_hz_focus_2: AudioStreamPlayer = $"40hzFocus2" # L100hz R140hz
