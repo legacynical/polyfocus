60699d26f2f24eba4603338530141588366949ce
Legacynical <legacynical@gmail.com>
2024-10-17 12:15:45 -0700

    refactor audio_manager initializations

--- globals/audio manager/audio_manager.gd
+++ globals/audio manager/audio_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
-@onready var click_basic: AudioStreamPlayer = $ClickBasic
-@onready var timer_complete: AudioStreamPlayer = $TimerComplete
-@onready var _40_hz_focus: AudioStreamPlayer = $"40hzFocus" # L250hz R290hz
-@onready var _40_hz_focus_2: AudioStreamPlayer = $"40hzFocus2" # L100hz R140hz
+@onready var click_basic: AudioStreamPlayer = %ClickBasic
+@onready var timer_complete: AudioStreamPlayer = %TimerComplete
+@onready var _40_hz_focus_100: AudioStreamPlayer = %"40hzFocus100" # L100hz R140hz
+@onready var _40_hz_focus_250: AudioStreamPlayer = %"40hzFocus250" # L250hz R290hz
+@onready var _40_hz_focus_400: AudioStreamPlayer = %"40hzFocus400" # L400hz R440hz
