0e7d583fc05eb926cc41ef4d8366c4162eb54952
Legacynical <legacynical@gmail.com>
2024-10-19 17:32:38 -0700

    add some new audio

--- globals/audio manager/audio_manager.gd
+++ globals/audio manager/audio_manager.gd
@@ -2,6 +2,13 @@ extends Node
 
 @onready var click_basic: AudioStreamPlayer = %ClickBasic
 @onready var timer_complete: AudioStreamPlayer = %TimerComplete
+@onready var metal_pipe: AudioStreamPlayer = %MetalPipe
 @onready var _40_hz_focus_100: AudioStreamPlayer = %"40hzFocus100" # L100hz R140hz
 @onready var _40_hz_focus_250: AudioStreamPlayer = %"40hzFocus250" # L250hz R290hz
 @onready var _40_hz_focus_400: AudioStreamPlayer = %"40hzFocus400" # L400hz R440hz
+@onready var time_to_focus_mb: AudioStreamPlayer = %TimeToFocusMB
+@onready var time_to_break_mb: AudioStreamPlayer = %TimeToBreakMB
+@onready var notification_mb: AudioStreamPlayer = %NotificationMB
+@onready var alert_1_mb: AudioStreamPlayer = %Alert1MB
+@onready var alert_2_mb: AudioStreamPlayer = %Alert2MB
+@onready var focus_skip: AudioStreamPlayer = %FocusSkip
