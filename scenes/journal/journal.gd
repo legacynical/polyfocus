48885d5eeb811b9ed6cc9ef719e36c91e16d5861
Legacynical <legacynical@gmail.com>
2024-10-20 14:11:21 -0700

    patch startup dependency issue on export build

--- scenes/journal/journal.gd
+++ scenes/journal/journal.gd
@@ -4,7 +4,7 @@ extends Panel
 var journal_data: Dictionary = {}
 
 # Path to the save file
-var save_file_path: String = "user://save/journals/journal_data.json"
+#var save_file_path: String = "user://save/journals/journal_data.json"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,20 +13,20 @@ func _ready() -> void:
 	pass
 
 # Function to save the journal data to a file
-func save_journal_data() -> void:
-	var file: FileAccess = FileAccess.open(save_file_path, FileAccess.WRITE)
-	if file:
-		file.store_string(JSON.stringify(journal_data))
-		file.close()
+#func save_journal_data() -> void:
+	#var file: FileAccess = FileAccess.open(save_file_path, FileAccess.WRITE)
+	#if file:
+		#file.store_string(JSON.stringify(journal_data))
+		#file.close()
 
 # Function to load the journal data from a file
-func load_journal_data() -> void:
-	if FileAccess.file_exists(save_file_path):
-		var file: FileAccess = FileAccess.open(save_file_path, FileAccess.READ)
-		if file:
-			var json_data: String = file.get_as_text()
-			journal_data = JSON.parse_string(json_data).result
-			file.close()
+#func load_journal_data() -> void:
+	#if FileAccess.file_exists(save_file_path):
+		#var file: FileAccess = FileAccess.open(save_file_path, FileAccess.READ)
+		#if file:
+			#var json_data: String = file.get_as_text()
+			#journal_data = JSON.parse_string(json_data).result
+			#file.close()
 
 ## TODO make a functional entry system
 #func _process(_delta: float) -> void:
