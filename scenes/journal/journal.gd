6751b7ad4471bfe7dddf9b8023a100fb34810340
Legacynical <legacynical@gmail.com>
2024-11-01 08:44:04 -0700

    refactor journal related scripts/scenes
    plans to consolidate journal save files into godot resource format

--- scenes/journal/journal.gd
+++ scenes/journal/journal.gd
@@ -3,8 +3,12 @@ extends Panel
 # Dictionary to hold the journal data
 var journal_data: Dictionary = {}
 
+@onready var entry_title: LineEdit = %EntryTitle
+@onready var entry_date: Label = %CurrentDateTime
+@onready var entry_text: TextEdit = %EntryText
+
 # Path to the save file
-#var save_file_path: String = "user://save/journals/journal_data.json"
+var save_file_path: String = "user://save/journals/journal_data.json"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,12 +17,41 @@ func _ready() -> void:
 	pass
 
 # Function to save the journal data to a file
-#func save_journal_data() -> void:
-	#var file: FileAccess = FileAccess.open(save_file_path, FileAccess.WRITE)
-	#if file:
-		#file.store_string(JSON.stringify(journal_data))
-		#file.close()
+func save_journal_data() -> void:
+	var file: FileAccess = FileAccess.open(save_file_path, FileAccess.WRITE)
+	if file:
+		file.store_string(JSON.stringify(journal_data))
+		file.close()
 
+func save_journal() -> void:
+	# Godot guarantees write access to user:// when exporting
+	var file: FileAccess = FileAccess.open("user://journal_entry.json", FileAccess.WRITE)
+	
+	var saved_entry: Dictionary = {}
+	
+	saved_entry["entry_title"] = entry_title.text
+	saved_entry["entry_date"] = entry_date.text
+	saved_entry["entry_text"] = entry_text.text
+	
+	# attempts to convert any Godot variable into JSON
+	var json: String = JSON.stringify(saved_entry)
+	file.store_string(json)
+	file.close()
+	
+func load_journal() -> void:
+	var file: FileAccess = FileAccess.open("user://journal_entry.json", FileAccess.READ)
+	
+	# reads whole file into a string
+	var json: String = file.get_as_text()
+	
+	# retrieves saved dictionary
+	var saved_entry: Dictionary = JSON.parse_string(json)
+	
+	entry_title.text = saved_entry["entry_title"]
+	entry_date.text = saved_entry["entry_date"]
+	entry_text.text = saved_entry["entry_text"] 
+	
+	file.close()	
 # Function to load the journal data from a file
 #func load_journal_data() -> void:
 	#if FileAccess.file_exists(save_file_path):
@@ -29,10 +62,3 @@ func _ready() -> void:
 			#file.close()
 
 ## TODO make a functional entry system
-#func _process(_delta: float) -> void:
-	#if Input.is_action_just_pressed("ui_accept"):
-		#var current_time: Dictionary = Time.get_datetime_dict_from_system()
-		#var date_str: String = (
-			#str(current_time.year) + "-" + 
-			#str(current_time.month).pad_zeros(2) + "-" + 
-			#str(current_time.day).pad_zeros(2))
