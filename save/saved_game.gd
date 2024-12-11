0557d550100e8308f95a47f0bb001d91f414df83
Legacynical <legacynical@gmail.com>
2024-12-11 01:02:06 -0800

    add more user settings

--- save/saved_game.gd
+++ save/saved_game.gd
@@ -2,12 +2,16 @@ class_name SavedGame
 extends Resource
 
 @export var default_window_size: Vector2 = Vector2(480, 270)
+
 @export var default_window_position: Vector2
 @export var window_position: Vector2
 @export var window_size: Vector2
 
 @export var default_focus_color: Color = Color.html("6fbd90")
 @export var default_break_color: Color = Color.html("5370a6")
+
+@export var focus_color: Color
+@export var break_color: Color
 @export var focus_color_presets: Array
 @export var break_color_presets: Array
 @export var is_progressive_pomo: bool
