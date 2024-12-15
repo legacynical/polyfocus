7a25f28e91cc41c7f6b4c881b4f2c38e7c69160c
Legacynical <legacynical@gmail.com>
2024-12-15 13:53:50 -0800

    add additional pomo settings
    also refactor ui elements

--- scenes/pomodoro timer/PomodoroTimer.gd
+++ scenes/pomodoro timer/PomodoroTimer.gd
@@ -28,7 +28,7 @@ extends Node
 
 @onready var saved_game: SavedGame = SavedGame.new()
 #@onready var save_file: String = "user://savegame.tres"
-@onready var save_file: String = "user://[v0.4.1-beta]savegame.tres"
+@onready var save_file: String = "user://[v0.4.2-beta]savegame.tres"
 
 
 var default_window_size: Vector2 = Vector2(480, 270)
