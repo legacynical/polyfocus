b04a54aa9ea910b7658d76a73f08c05bc110f993
Legacynical <legacynical@gmail.com>
2025-01-24 13:39:24 -0800

    add checkmark animation

--- scenes/to-do list/to_do_list_entry.gd
+++ scenes/to-do list/to_do_list_entry.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export var to_do_edit: LineEdit
-@export var to_do_label: RichTextLabel
 
-func _on_line_edit_text_changed(new_text) -> void:
-	to_do_label.text = new_text
+
+func _on_texture_button_toggled(toggled_on):
+	pass # Replace with function body.
