6d621cde02db8583e4d4b206eee988844876f5f3
Legacynical <legacynical@gmail.com>
2025-01-25 18:44:18 -0800

    add checkbox animation

--- scenes/to-do list/to_do_list_entry.gd
+++ scenes/to-do list/to_do_list_entry.gd
@@ -1,7 +1,12 @@
 extends Control
 
 @export var to_do_edit: LineEdit
+@export var check_box: TextureButton
+@export var animation: AnimationPlayer
 
-
-func _on_texture_button_toggled(toggled_on):
-	pass # Replace with function body.
+func _on_texture_button_toggled(toggled_on) -> void:
+	if toggled_on:
+		animation.play("checkmark")
+	else:
+		animation.play_backwards("checkmark")
+		
